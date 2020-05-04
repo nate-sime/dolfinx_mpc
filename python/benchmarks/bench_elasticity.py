9e8e7b564f19965b97c7a359f08fd5b6f5c84af1
Jorgen Dokken <jsd55@cockatoo.bpi.cam.ac.uk>
2020-05-04 10:59:10 +0100

    Updates to benchmark elasticity

--- python/benchmarks/bench_elasticity.py
+++ python/benchmarks/bench_elasticity.py
@@ -12,6 +12,8 @@ from contextlib import ExitStack
 import dolfinx
 import dolfinx.io
 import dolfinx.la
+import dolfinx.log
+import dolfinx.common
 import dolfinx.mesh
 import dolfinx_mpc
 import dolfinx_mpc.utils
@@ -67,7 +69,10 @@ def build_elastic_nullspace(V):
 def demo_elasticity(r_lvl=1):
     mesh = dolfinx.UnitCubeMesh(MPI.COMM_WORLD, 4, 4, 4)
     for i in range(r_lvl):
+        # dolfinx.log.set_log_level(dolfinx.log.LogLevel.INFO)
         mesh = dolfinx.mesh.refine(mesh, redistribute=True)
+        # dolfinx.log.set_log_level(dolfinx.log.LogLevel.ERROR)
+
     fdim = mesh.topology.dim - 1
     V = dolfinx.VectorFunctionSpace(mesh, ("Lagrange", 1))
 
@@ -199,7 +204,7 @@ def demo_elasticity(r_lvl=1):
     def org_monitor(ksp, its, rnorm, r_lvl=-1):
         if MPI.COMM_WORLD.rank == 0:
             print("Orginal: {}: Iteration: {}, rel. residual: {}".format(
-                r_lvl, its, rnorm))
+                  r_lvl, its, rnorm))
 
     def org_pmonitor(ksp, its, rnorm):
         return org_monitor(ksp, its, rnorm, r_lvl=r_lvl)
@@ -217,47 +222,54 @@ def demo_elasticity(r_lvl=1):
     outfile.close()
 
     # Create global transformation matrix
-    K = dolfinx_mpc.utils.create_transformation_matrix(V.dim(), slaves,
-                                                       masters, coeffs,
-                                                       offsets)
+    # K = dolfinx_mpc.utils.create_transformation_matrix(V.dim(), slaves,
+    #                                                    masters, coeffs,
+    #                                                    offsets)
 
-    # Create reduced A
-    A_global = dolfinx_mpc.utils.PETScMatrix_to_global_numpy(A_org)
-    reduced_A = np.matmul(np.matmul(K.T, A_global), K)
+    # # Create reduced A
+    # A_global = dolfinx_mpc.utils.PETScMatrix_to_global_numpy(A_org)
+    # reduced_A = np.matmul(np.matmul(K.T, A_global), K)
 
-    # Created reduced L
-    vec = dolfinx_mpc.utils.PETScVector_to_global_numpy(L_org)
-    reduced_L = np.dot(K.T, vec)
+    # # Created reduced L
+    # vec = dolfinx_mpc.utils.PETScVector_to_global_numpy(L_org)
+    # reduced_L = np.dot(K.T, vec)
 
-    # Solve linear system
-    d = np.linalg.solve(reduced_A, reduced_L)
+    # # Solve linear system
+    # d = np.linalg.solve(reduced_A, reduced_L)
 
-    # Back substitution to full solution vector
-    uh_numpy = np.dot(K, d)
+    # # Back substitution to full solution vector
+    # uh_numpy = np.dot(K, d)
 
-    # Transfer data from the MPC problem to numpy arrays for comparison
-    A_mpc_np = dolfinx_mpc.utils.PETScMatrix_to_global_numpy(A)
-    mpc_vec_np = dolfinx_mpc.utils.PETScVector_to_global_numpy(b)
+    # # Transfer data from the MPC problem to numpy arrays for comparison
+    # A_mpc_np = dolfinx_mpc.utils.PETScMatrix_to_global_numpy(A)
+    # mpc_vec_np = dolfinx_mpc.utils.PETScVector_to_global_numpy(b)
 
-    # Compare LHS, RHS and solution with reference values
-    dolfinx_mpc.utils.compare_matrices(reduced_A, A_mpc_np, slaves)
-    dolfinx_mpc.utils.compare_vectors(reduced_L, mpc_vec_np, slaves)
-    assert np.allclose(uh.array, uh_numpy[uh.owner_range[0]:uh.owner_range[1]])
+    # # Compare LHS, RHS and solution with reference values
+    # dolfinx_mpc.utils.compare_matrices(reduced_A, A_mpc_np, slaves)
+    # dolfinx_mpc.utils.compare_vectors(reduced_L, mpc_vec_np, slaves)
+    # assert np.allclose(uh.array, uh_numpy[uh.owner_range[0]:uh.owner_range[1]])
 
-    for i in range(len(masters)):
-        if uh.owner_range[0] < masters[i] and masters[i] < uh.owner_range[1]:
-            print("-"*10, i, "-"*10)
-            print("MASTER DOF, MPC {0:.4e} Unconstrained {1:.4e}"
-                  .format(uh.array[masters[i]-uh.owner_range[0]],
-                          u_.vector.array[masters[i]-uh.owner_range[0]]))
-            print("Slave (given as master*coeff) {0:.4e}".
-                  format(uh.array[masters[i]-uh.owner_range[0]]*coeffs[0]))
+    # for i in range(len(masters)):
+    #     if uh.owner_range[0] < masters[i] and masters[i] < uh.owner_range[1]:
+    #         print("-"*10, i, "-"*10)
+    #         print("MASTER DOF, MPC {0:.4e} Unconstrained {1:.4e}"
+    #               .format(uh.array[masters[i]-uh.owner_range[0]],
+    #                       u_.vector.array[masters[i]-uh.owner_range[0]]))
+    #         print("Slave (given as master*coeff) {0:.4e}".
+    #               format(uh.array[masters[i]-uh.owner_range[0]]*coeffs[0]))
 
-        if uh.owner_range[0] < slaves[i] and slaves[i] < uh.owner_range[1]:
-            print("SLAVE  DOF, MPC {0:.4e} Unconstrained {1:.4e}"
-                  .format(uh.array[slaves[i]-uh.owner_range[0]],
-                          u_.vector.array[slaves[i]-uh.owner_range[0]]))
+    #     if uh.owner_range[0] < slaves[i] and slaves[i] < uh.owner_range[1]:
+    #         print("SLAVE  DOF, MPC {0:.4e} Unconstrained {1:.4e}"
+    #               .format(uh.array[slaves[i]-uh.owner_range[0]],
+    #                       u_.vector.array[slaves[i]-uh.owner_range[0]]))
 
 
 if __name__ == "__main__":
-    demo_elasticity()
+    for i in range(6):
+        # dolfinx.log.set_log_level(dolfinx.log.LogLevel.INFO)
+        # dolfinx.log.log(dolfinx.log.LogLevel.INFO,
+        #                 "Run {0:1d} in progress".format(i))
+        # dolfinx.log.set_log_level(dolfinx.log.LogLevel.ERROR)
+        demo_elasticity(i)
+        # dolfinx.common.list_timings(
+        #     MPI.COMM_WORLD, [dolfinx.common.TimingType.wall])
